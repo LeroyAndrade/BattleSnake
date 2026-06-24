43eaa641765922871532cebf85bf336fe22774c3
LeroyAndrade <leroy_andrade@hotmail.com>
2026-06-24 03:26:43 +0200

    Self check toegevoegd

--- src/main/java/nl/hu/bep/setup/SnakeGame/Model/Game.java
+++ src/main/java/nl/hu/bep/setup/SnakeGame/Model/Game.java
@@ -1,8 +1,11 @@
 package nl.hu.bep.setup.SnakeGame.Model;
 
+import java.util.List;
+import java.util.Map;
+
 public class Game {
 
-    public String chooseMove(int myX, int myY, int boardWidth, int boardHeight) {
+    public String chooseMove(int myX, int myY, int boardWidth, int boardHeight,  List<Map<String, Object>> myBody ) {
         Board board = new Board(boardWidth, boardHeight);
 
         // Check eerst of up veilig is
@@ -25,7 +28,22 @@ public class Game {
             return "left";
         }
 
-        // Als echt niks kan, geef toch iets terug
+        // als default:
         return "up";
+    }
+
+    private boolean hitsOwnBody(int newX, int newY, List<Map<String, Object>> myBody) {
+        // Loop door alle stukjes van je eigen lichaam
+        for (Map<String, Object> bodyPart : myBody) {
+            int bodyX = (int) bodyPart.get("x");
+            int bodyY = (int) bodyPart.get("y");
+
+            // Als de nieuwe plek hetzelfde is als een body-part, dan return true
+            if (newX == bodyX && newY == bodyY) {
+                return true;
+            }
+        }
+        // Geen body-part geraakt
+        return false;
     }
 }
--- src/main/java/nl/hu/bep/setup/SnakeGame/webservices/BattlesnakeResource.java
+++ src/main/java/nl/hu/bep/setup/SnakeGame/webservices/BattlesnakeResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 import java.util.Map;
 
 @Path("/")
@@ -61,8 +62,11 @@ public class BattlesnakeResource {
         int myX = (int) head.get("x");
         int myY = (int) head.get("y");
 
+
+        List<Map<String, Object>> myBody = (List<Map<String, Object>>) you.get("body");
+
         Game game = new Game();
-        String move = game.chooseMove(myX, myY, boardWidth, boardHeight);
+        String move = game.chooseMove(myX, myY, boardWidth, boardHeight, myBody);
 
         System.out.println("Mijn hoofd x = " + myX);
         System.out.println("Mijn hoofd y = " + myY);
