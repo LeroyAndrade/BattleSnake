6a35c23bbb9fbdf3d4aa695443fec0c55f1f4e7e
LeroyAndrade <leroy_andrade@hotmail.com>
2026-06-26 01:48:47 +0200

    Getters en setters

--- src/main/java/nl/hu/bep/setup/SnakeGame/Model/SnakeSettings.java
+++ src/main/java/nl/hu/bep/setup/SnakeGame/Model/SnakeSettings.java
@@ -2,14 +2,16 @@
 //bewaar  color, head, tail en version.
 package nl.hu.bep.setup.SnakeGame.Model;
 
-public class SnakeSettings {
+import java.io.Serializable;
+
+public class SnakeSettings implements Serializable {
     private String author;
     private String color;
     private String head;
     private String tail;
     private String version;
 
-    private static SnakeSettings current = new SnakeSettings();
+    private SnakeSettings current = new SnakeSettings();
 
     public SnakeSettings() {
         this.author = "LeroyAndrade";
@@ -17,5 +19,41 @@ public class SnakeSettings {
         this.head = "default";
         this.tail = "default";
         this.version = "000.000.001-beta";
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public void setTail(String tail) {
+        this.tail = tail;
+    }
+
+    public SnakeSettings getCurrent() {
+        return current;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getTail() {
+        return tail;
+    }
+
+    public String getVersion() {
+        return version;
     }
 }
