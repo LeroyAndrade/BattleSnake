92d59659f913a77c882e0a27d06e367d78953153
LeroyAndrade <leroy_andrade@hotmail.com>
2026-06-26 02:19:38 +0200

    BattleSnake data laden

--- src/main/java/nl/hu/bep/setup/SnakeGame/persistence/PersistenceManager.java
+++ src/main/java/nl/hu/bep/setup/SnakeGame/persistence/PersistenceManager.java
@@ -31,6 +31,37 @@ public class PersistenceManager implements ServletContextListener {
         appDataOpslag = nieuweAppDataOpslag;
     }
 
+    public static void loadAppDataFromFile() {
+        try {
+            // Ik controleer eerst of het bestand battlesnake-data.obj bestaat.
+            // Als het bestand niet bestaat, hoeft er niks ingeladen te worden.
+            if (Files.exists(BATTLESNAKE_STORAGE)) {
+
+                // Ik open een InputStream naar het bestand.
+                InputStream is = Files.newInputStream(BATTLESNAKE_STORAGE);
+
+                // Met ObjectInputStream kan ik een opgeslagen Java-object teruglezen.
+                ObjectInputStream ois = new ObjectInputStream(is);
+
+                // Ik lees het object uit het bestand en cast het terug naar AppDataOpslag.
+                AppDataOpslag loadedAppDataOpslag = (AppDataOpslag) ois.readObject();
+
+                // Ik zet de ingeladen AppDataOpslag als de huidige data.
+                PersistenceManager.setAppDataOpslag(loadedAppDataOpslag);
+
+                // Ik sluit de streams weer.
+                ois.close();
+                is.close();
+
+                System.out.println("AppDataOpslag is ingeladen uit bestand.");
+            }
+        } catch (Exception e) {
+            // Als het laden niet lukt, print ik de foutmelding.
+            // De standaard AppDataOpslag blijft dan gewoon gebruikt worden.
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         System.out.println("App aan het opstarten");
